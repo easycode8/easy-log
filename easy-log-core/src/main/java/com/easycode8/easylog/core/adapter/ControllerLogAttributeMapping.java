34c0b38dab12cccd9793e9ff3b4712099fd229e6
easycode8 <you@example.com>
2023-07-13 09:25:56 +0800

    【优化】easy-log-core忽略controller继承父类的方法

--- easy-log-core/src/main/java/com/easycode8/easylog/core/adapter/ControllerLogAttributeMapping.java
+++ easy-log-core/src/main/java/com/easycode8/easylog/core/adapter/ControllerLogAttributeMapping.java
@@ -42,6 +42,10 @@ public class ControllerLogAttributeMapping implements LogAttributeMappingAdapter
 
 
     private boolean isControllerPublicMethod(Method method, Class<?> targetClass) {
+        // 如果方法不是controller自己的而是继承来的则忽略
+        if (!method.getDeclaringClass().equals(targetClass)) {
+            return false;
+        }
         return (targetClass.getAnnotation(Controller.class) != null && (method.getAnnotation(ResponseBody.class) != null || method.getReturnType() == ResponseEntity.class))
                 ||
                 (targetClass.getAnnotation(RestController.class) != null && !Modifier.isStatic(method.getModifiers())
