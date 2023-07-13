713600d32819800101100672ee5874f0b7286fac
easycode8 <you@example.com>
2023-07-13 09:15:27 +0800

    【优化】easy-log-core补充打印日志属性缓存类型

--- easy-log-core/src/main/java/com/easycode8/easylog/core/cache/LogAttributeCacheConfiguration.java
+++ easy-log-core/src/main/java/com/easycode8/easylog/core/cache/LogAttributeCacheConfiguration.java
@@ -1,6 +1,8 @@
 package com.easycode8.easylog.core.cache;
 
 import com.easycode8.easylog.core.annotation.EasyLogProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 public abstract class LogAttributeCacheConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogAttributeCacheConfiguration.class);
 
     @ConditionalOnProperty(value = "spring.easy-log.cache.redis.enabled", havingValue = "true", matchIfMissing = true)
     @ConditionalOnClass(StringRedisTemplate.class)
@@ -17,6 +20,7 @@ public abstract class LogAttributeCacheConfiguration {
         @ConditionalOnBean(StringRedisTemplate.class)
         @ConditionalOnMissingBean(LogAttributeCache.class)
         public LogAttributeCache logAttributeCache (StringRedisTemplate stringRedisTemplate, EasyLogProperties easyLogProperties) {
+            LOGGER.info("[easy-log]日志属性--使用redis缓存");
             return new LogAttributeRedisCache(stringRedisTemplate, easyLogProperties);
         }
     }
@@ -25,6 +29,7 @@ public abstract class LogAttributeCacheConfiguration {
         @Bean
         @ConditionalOnMissingBean(LogAttributeCache.class)
         public LogAttributeCache logAttributeCache () {
+            LOGGER.info("[easy-log]日志属性--使用本地内存缓存");
             return new LogAttributeMemoryCache();
         }
     }
